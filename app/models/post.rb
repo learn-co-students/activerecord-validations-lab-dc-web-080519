1276a00af723328a4ea4a395d6cd171620f594c4
Pete Hanner <petehanner@gmail.com>
2019-09-06 13:47:54 -0400

    validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :non_clickbait
 end
+
+def non_clickbait
+   if self.title.present? && !self.title.downcase.include?("won't believe" || "secret" || "top /\d/" || "guess")
+     errors.add(:title, "Not clickbait enough!")
+   end
+ end
