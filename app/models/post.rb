920450ef16cff7a9e1b12c43747e0f17170e5544
github username <github email address>
2019-09-08 08:56:15 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ActiveRecord::Base
-  # belongs_to :author
-  validates :title, presence: true
 
+  validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validate :clickbaity?
 
   def clickbaity?
+    
     baits = [
     /Won't Believe/,
     /Secret/,
@@ -18,18 +18,6 @@ class Post < ActiveRecord::Base
       errors.add(:title, "is not clickbaity enough")
     end
   end
-
-  # def clickbaity
-  #   clickbait = %W(#{"Won't Believe"} Secret Top Guess)
-
-
-  # end
-
-#title present && clickbaity -> ["Won't Believe", "Secret", "Top [number]", "Guess"]
-#content is >= 250 charachters
-#summary is <= 250 charachters
-#category is Fiction || Non-Fiction (inclusion)
-
 
 
 end
